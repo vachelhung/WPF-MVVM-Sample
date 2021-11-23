ff0f46d7b4cd48fc7bb0f5d03fe3b09834d05922
培原 洪 <vachelhung@gmail.com>
2021-11-23 10:31:12 +0800

    加入專案檔案。

---
+++ GetSetTest20211123/Models/MainModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetTest20211123.Models
+{
+    public class Calc
+    {
+        public int count { get; set; }
+        public int money { get; set; }
+        public int total { get { return count * money; } }
+    }
+}
---
+++ GetSetTest20211123/ViewModels/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using BindingLibrary;
+using GetSetTest20211123.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetTest20211123.ViewModels
+{
+    internal class MainViewModel : NotifyPropertyBase
+    {
+        public Calc calc = new();
+        
+        public int Count
+        {
+            get { return calc.count; }
+            set
+            {
+                if (calc.count != value)
+                {
+                    calc.count = value;
+                    OnPropertyChanged("total");
+                }
+            }
+        }
+
+        public int Money
+        {
+            get { return calc.money; }
+            set
+            {
+                if (calc.money != value)
+                {
+                    calc.money = value;
+                    OnPropertyChanged("total");
+                }
+            }
+        }
+
+        public int Total
+        {
+            get { return calc.total; }
+        }
+    }
+}
